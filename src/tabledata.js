bc8d1ee950796e824297a7260bf393fb03dd1564
Ajith <ajithkumar000@gmail.com>
2023-04-05 19:38:56 +0530

    change

--- src/tabledata.js
+++ src/tabledata.js
@@ -1,291 +1,302 @@
-import { Table, Avatar,Image,Badge,Checkbox,Tag,Button,Space,Select,AutoComplete} from 'antd';
-import { ProfileFilled } from '@ant-design/icons';
-import { useState } from 'react';
+import {
+  Table,
+  Avatar,
+  Image,
+  Badge,
+  Checkbox,
+  Tag,
+  Button,
+  Space,
+  Select,
+  AutoComplete,
+} from "antd";
+import { ProfileFilled } from "@ant-design/icons";
+import { useState } from "react";
 
 const value = (str, repeat = 1) => ({
-    value: str.repeat(repeat),
+  value: str.repeat(repeat),
 });
 
 const columns = [
-    {
-        title:'CHECKBOX',
-        dataIndex:'checkbox',
-        
-        render: (value) => {
-            return (
-                <Checkbox></Checkbox>  
-            );
-        }, 
-        colSpan: 1,
-    }, 
+  {
+    title: "CHECKBOX",
+    dataIndex: "checkbox",
 
-    {
-        title:'#',
-        dataIndex:'sno',
-        align: "left",
-        sorter: (a, b) => a.sno - b.sno,
+    render: (value) => {
+      return <Checkbox></Checkbox>;
     },
-{
-    title: 'PROJECT NAME',
-    dataIndex: 'projectname',
+    colSpan: 1,
+  },
+
+  {
+    title: "#",
+    dataIndex: "sno",
+    align: "left",
+    sorter: (a, b) => a.sno - b.sno,
+  },
+  {
+    title: "PROJECT NAME",
+    dataIndex: "projectname",
     sorter: (a, b) => a.projectname - b.projectname,
-    },
-    
-{
-    title: 'PM', 
-    dataIndex: 'pm',
+  },
+
+  {
+    title: "PM",
+    dataIndex: "pm",
     sorter: (a, b) => a.pm - b.pm,
     render: () => {
-        return (
-        <Avatar className='avatar' 
-        icon={<Image width={20}
-            src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"></Image>}>    
-        </Avatar>
-        );
+      return (
+        <Avatar
+          className="avatar"
+          icon={
+            <Image
+              width={20}
+              src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"
+            ></Image>
+          }
+        ></Avatar>
+      );
     },
-},
+  },
 
-{
-    title: 'STATUS',
-    dataIndex: 'status',
+  {
+    title: "STATUS",
+    dataIndex: "status",
     sorter: (a, b) => a.status - b.status,
-    render:(value)=>{
-        return(
-            <Badge color="green" text="on track"/>
-            
-        )
+    render: (value) => {
+      return <Badge color="green" text="on track" />;
     },
-    },
-{
-    title: 'LAST UPDATE',
-    dataIndex: 'lastupdate',
+  },
+  {
+    title: "LAST UPDATE",
+    dataIndex: "lastupdate",
     sorter: (a, b) => a.lastupdate - b.lastupdate,
-    },
+  },
 
-    
-
-{
-    title: 'RESOURCES',
-    dataIndex: 'resources',
+  {
+    title: "RESOURCES",
+    dataIndex: "resources",
     sorter: (a, b) => a.resources - b.resources,
-    render:(resources)=>{
-        return(
-            <Tag color="#808080" key={resources}>
-            {resources}
+    render: (resources) => {
+      return (
+        <Tag color="#808080" key={resources}>
+          {resources}
         </Tag>
-        )
+      );
     },
-    
-},
+  },
 
-{
-    title: 'PROJECT TIMELINE',
-    dataIndex: 'projecttimeline',
+  {
+    title: "PROJECT TIMELINE",
+    dataIndex: "projecttimeline",
     sorter: (a, b) => a.projecttimeline - b.projecttimeline,
-    render:(projecttimeline)=>{
-        return(
-            <Tag color="#808080" key={projecttimeline}>
-            {projecttimeline}
+    render: (projecttimeline) => {
+      return (
+        <Tag color="#808080" key={projecttimeline}>
+          {projecttimeline}
         </Tag>
-        )
+      );
     },
-},
+  },
 
-{
-    title: 'ESTIMATION',
-    dataIndex: 'estimation',
+  {
+    title: "ESTIMATION",
+    dataIndex: "estimation",
     sorter: (a, b) => a.estimation - b.estimation,
-},
-
+  },
 ];
 
 const data = [
-{
-    key: '1',
+  {
+    key: "1",
     sno: 1,
-    projectname: 'Allosaurus web app',
-    lastupdate:'15 MAR 2021, 12.47 PM' ,
-    resources:['3'],
-    projecttimeline:'15 May 2021 > 15 Aug 2021',
-    estimation:'US$ 10.5K',
-
-},
-{
-    key: '2',
+    projectname: "Allosaurus web app",
+    lastupdate: "15 MAR 2021, 12.47 PM",
+    resources: ["3"],
+    projecttimeline: "15 May 2021 > 15 Aug 2021",
+    estimation: "US$ 10.5K",
+  },
+  {
+    key: "2",
     sno: 2,
-    projectname: 'MicroRaptor website',
-    lastupdate:'15 MAR 2021, 12.47 PM' ,
-    resources:['3'],
-    projecttimeline:'15 May 2021 > 15 Aug 2021',
-    estimation:'US$ 10.5K',
-
-},
-{
-    key: '3',
+    projectname: "MicroRaptor website",
+    lastupdate: "15 MAR 2021, 12.47 PM",
+    resources: ["3"],
+    projecttimeline: "15 May 2021 > 15 Aug 2021",
+    estimation: "US$ 10.5K",
+  },
+  {
+    key: "3",
     sno: 3,
-    projectname: 'Tarius landing page',
-    status: 'ON TRACK',
-    lastupdate:'15 MAR 2021, 12.47 PM' ,
-    resources:['+'],
-    projecttimeline:'_______________________________',
-    estimation:'US$ 10.5K',
-},
-{
-    key: '4',
+    projectname: "Tarius landing page",
+    status: "ON TRACK",
+    lastupdate: "15 MAR 2021, 12.47 PM",
+    resources: ["+"],
+    projecttimeline: "_______________________________",
+    estimation: "US$ 10.5K",
+  },
+  {
+    key: "4",
     sno: 4,
-    projectname: 'Rugops App',
-    status: 'ON TRACK',
-    lastupdate:'15 MAR 2021, 12.47 PM' ,
-    resources:'3',
-    projecttimeline:'15 May 2021 > 15 Aug 2021',
-    estimation:'US$ 10.5K',
-},
-{
-    key: '5',
+    projectname: "Rugops App",
+    status: "ON TRACK",
+    lastupdate: "15 MAR 2021, 12.47 PM",
+    resources: "3",
+    projecttimeline: "15 May 2021 > 15 Aug 2021",
+    estimation: "US$ 10.5K",
+  },
+  {
+    key: "5",
     sno: 5,
-    projectname: 'Erketu',
-    status: 'ON TRACK',
-    lastupdate:'15 MAR 2021, 12.47 PM' ,
-    resources:'3',
-    projecttimeline:'15 May 2021 > 15 Aug 2021',
-    estimation:'US$ 10.5K',
-},
-{
-    key: '6',
+    projectname: "Erketu",
+    status: "ON TRACK",
+    lastupdate: "15 MAR 2021, 12.47 PM",
+    resources: "3",
+    projecttimeline: "15 May 2021 > 15 Aug 2021",
+    estimation: "US$ 10.5K",
+  },
+  {
+    key: "6",
     sno: 6,
-    projectname: 'Capricon',
-    status: 'ON TRACK',
-    lastupdate:'15 MAR 2021, 12.47 PM' ,
-    resources:'3',
-    projecttimeline:'15 May 2021 > 15 Aug 2021',
-    estimation:'US$ 10.5K',
-},
-{
-    key: '7',
+    projectname: "Capricon",
+    status: "ON TRACK",
+    lastupdate: "15 MAR 2021, 12.47 PM",
+    resources: "3",
+    projecttimeline: "15 May 2021 > 15 Aug 2021",
+    estimation: "US$ 10.5K",
+  },
+  {
+    key: "7",
     sno: 7,
-    projectname: 'Sagittarius',
-    status: 'ON TRACK',
-    lastupdate:'15 MAR 2021, 12.47 PM' ,
-    resources:['+'],
-    projecttimeline:'15 May 2021 > 15 Aug 2021',
-    estimation:'US$ 10.5K',
-},
-{
-    key: '8',
+    projectname: "Sagittarius",
+    status: "ON TRACK",
+    lastupdate: "15 MAR 2021, 12.47 PM",
+    resources: ["+"],
+    projecttimeline: "15 May 2021 > 15 Aug 2021",
+    estimation: "US$ 10.5K",
+  },
+  {
+    key: "8",
     sno: 8,
-    projectname: 'Gemini',
-    status: 'ON TRACK',
-    lastupdate:'15 MAR 2021, 12.47 PM' ,
-    resources:'3',
-    projecttimeline:'15 May 2021 > 15 Aug 2021',
-    estimation:'US$ 10.5K',
-},
-{
-    key: '9',
+    projectname: "Gemini",
+    status: "ON TRACK",
+    lastupdate: "15 MAR 2021, 12.47 PM",
+    resources: "3",
+    projecttimeline: "15 May 2021 > 15 Aug 2021",
+    estimation: "US$ 10.5K",
+  },
+  {
+    key: "9",
     sno: 9,
-    projectname: 'Pisces',
-    status: 'ON TRACK',
-    lastupdate:'15 MAR 2021, 12.47 PM' ,
-    resources:'3',
-    projecttimeline:'15 May 2021 > 15 Aug 2021',
-    estimation:'US$ 10.5K',
-},
-{
-    key: '10',
+    projectname: "Pisces",
+    status: "ON TRACK",
+    lastupdate: "15 MAR 2021, 12.47 PM",
+    resources: "3",
+    projecttimeline: "15 May 2021 > 15 Aug 2021",
+    estimation: "US$ 10.5K",
+  },
+  {
+    key: "10",
     sno: 10,
-    projectname: 'Taurus',
-    status: 'ON TRACK',
-    lastupdate:'15 MAR 2021, 12.47 PM' ,
-    resources:'3',
-    projecttimeline:'15 May 2021 > 15 Aug 2021',
-    estimation:'US$ 10.5K',
-},
-{
-    key: '11',
+    projectname: "Taurus",
+    status: "ON TRACK",
+    lastupdate: "15 MAR 2021, 12.47 PM",
+    resources: "3",
+    projecttimeline: "15 May 2021 > 15 Aug 2021",
+    estimation: "US$ 10.5K",
+  },
+  {
+    key: "11",
     sno: 11,
-    projectname: 'Osiris',
-    status: 'ON TRACK',
-    lastupdate:'15 MAR 2021, 12.47 PM' ,
-    resources:'3',
-    projecttimeline:'15 May 2021 > 15 Aug 2021',
-    estimation:'US$ 10.5K',
-},
-{
-    key: '12',
+    projectname: "Osiris",
+    status: "ON TRACK",
+    lastupdate: "15 MAR 2021, 12.47 PM",
+    resources: "3",
+    projecttimeline: "15 May 2021 > 15 Aug 2021",
+    estimation: "US$ 10.5K",
+  },
+  {
+    key: "12",
     sno: 12,
-    projectname: 'Horous',
-    status: 'ON TRACK',
-    lastupdate:'15 MAR 2021, 12.47 PM' ,
-    resources:'3',
-    projecttimeline:'15 May 2021 > 15 Aug 2021',
-    estimation:'US$ 10.5K',
-},
-{
-    key: '13',
+    projectname: "Horous",
+    status: "ON TRACK",
+    lastupdate: "15 MAR 2021, 12.47 PM",
+    resources: "3",
+    projecttimeline: "15 May 2021 > 15 Aug 2021",
+    estimation: "US$ 10.5K",
+  },
+  {
+    key: "13",
     sno: 13,
-    projectname: 'Hathor',
-    status: 'ON TRACK',
-    lastupdate:'15 MAR 2021, 12.47 PM' ,
-    resources:'3',
-    projecttimeline:'15 May 2021 > 15 Aug 2021',
-    estimation:'US$ 10.5K',
-},
+    projectname: "Hathor",
+    status: "ON TRACK",
+    lastupdate: "15 MAR 2021, 12.47 PM",
+    resources: "3",
+    projecttimeline: "15 May 2021 > 15 Aug 2021",
+    estimation: "US$ 10.5K",
+  },
 ];
 
 const Tabledata = () => {
-
-const [options, setOptions] = useState([]);
-const getPanelValue = (searchText) =>
+  const [options, setOptions] = useState([]);
+  const getPanelValue = (searchText) =>
     !searchText ? [] : [value(searchText)];
-const onSelect = (data) => {
-    console.log('onSelect', data);
-};
+  const onSelect = (data) => {
+    console.log("onSelect", data);
+  };
 
-    return (
+  return (
     <>
-        <Space>
-        <Button className='button1'><ProfileFilled/></Button>
-        <Select defaultValue="All"
-    options={[
-        {
-        value: 'Allosaurus web app',
-        label: 'Allosaurus web app',
-        },
-        {
-        value: 'MicroRaptor website',
-        label: 'MicroRaptor website',
-        },
-        {
-        value: 'Tarius landing page',
-        label: 'Tarius landing page',
-        },
-        {
-        value: 'Rugops App',
-        label: 'Rugops App',
-        },
-        {
-        value: 'Erketu',
-        label: 'Erketu',
+      <Space>
+        <Button className="button1">
+          <ProfileFilled />
+        </Button>
+        <Select
+          style={{ width: "150px" }}
+          defaultValue="All"
+          options={[
+            {
+              value: "Allosaurus web app",
+              label: "Allosaurus web app",
             },
-    ]}
-    className="select">
-    </Select>
+            {
+              value: "MicroRaptor website",
+              label: "MicroRaptor website",
+            },
+            {
+              value: "Tarius landing page",
+              label: "Tarius landing page",
+            },
+            {
+              value: "Rugops App",
+              label: "Rugops App",
+            },
+            {
+              value: "Erketu",
+              label: "Erketu",
+            },
+          ]}
+          className="select"
+        ></Select>
 
-    <AutoComplete
-        className='autocompelte'
-        options={options}
-        onSelect={onSelect}
-        onSearch={(text) => setOptions(getPanelValue(text))}
-        placeholder="Search"
-    />
+        <AutoComplete
+          style={{ width: "150px" }}
+          className="autocompelte"
+          options={options}
+          onSelect={onSelect}
+          onSearch={(text) => setOptions(getPanelValue(text))}
+          placeholder="Search"
+        />
 
-        <Button className='button'>+ New Project</Button>
-        </Space>
-        <Table columns={columns} dataSource={data} 
-        pagination={{showSizeChanger: true,size: "default",}}/>
+        <Button className="button">+ New Project</Button>
+      </Space>
+      <Table
+        columns={columns}
+        dataSource={data}
+        pagination={{ showSizeChanger: true, size: "default" }}
+      />
     </>
-    );
-}
-
+  );
+};
 
 export default Tabledata;
